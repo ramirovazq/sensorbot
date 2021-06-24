b661724bf208d761093b0e5680b2ad4907202354
Ramiro Baltazar Vazquez Sanchez <ramirovazquez95@gmail.com>
2021-06-23 19:06:15 -0500

    subprocess service

--- proyecto/servicios/telegram_demo.py
+++ proyecto/servicios/telegram_demo.py
@@ -2,11 +2,13 @@ if __name__ == '__main__':
     import time
     import os
     import subprocess
+
     TOKEN_DEMO = os.getenv("TOKEN_DEMO")
+
+    sub = subprocess.run(["systemctl", "--user", "start", "telegram_foto.service"])
+    print(sub)
+
     while True:
         print(f'Este es el DEMO de telegram script en python ejecutandose cada 5 segundos {TOKEN_DEMO}')
-        print(TOKEN_DEMO)
         print("-----")
-        algo = subprocess.run(["systemctl", "--user", "start", "telegram_sensor.service"])
-        print(algo)
-        time.sleep(50)
+        time.sleep(5)
